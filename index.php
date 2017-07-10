e83dc754b9ae8aacb494235a06b737a97792ad1f
XLabour <syarif.hidayatullah@gmail.com>
2017-07-10 14:40:02 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$LP_TITLE = getenv('LP_TITLE');
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Jual Domain Unik</title>
+</head>
+<style>
+    body{
+        font-family:arial, tahoma;
+        font-size:11pt;
+    }
+</style>
+
+<body>
+<center>Welcome to <?php echo $LP_TITLE;?></center>
+</body>
+</html>
