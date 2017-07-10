1384214aba2735261b45f02b01a0ef0961332379
XLabour <syarif.hidayatullah@gmail.com>
2017-07-10 14:57:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ $LP_TITLE = getenv('LP_TITLE');
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="XL Axiata">
+    <meta name="author" content="XL Axiata">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>|<?php echo $LP_TITLE;?></title>
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image//vnd.microsoft.icon" href="./img/favicon.ico">
 </head>
 <style>
 body{
@@ -16,6 +20,9 @@ body{
 </style>
 
 <body>
-<center>Welcome to <?php echo $LP_TITLE;?></center>
+<center>
+<img width="" src="./img/header.png" />
+Welcome to <?php echo $LP_TITLE;?>
+</center>
 </body>
 </html>
